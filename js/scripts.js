00f04ee21ebe7562a612a2026058d2b39928b159
Jaki Widby <jaki.widby@gmail.com>
2020-01-17 12:02:04 -0800

    create vars to collect values from form questions.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var name = $("input#name").val();
+var valOne = $("input:radio[name=toothbrush]:checked").val();
+var valTwo = $("input:radio[name=season]:checked").val();
+var valThree = $("input:radio[name=shape]:checked").val();
+var valFour = $("input:radio[name=creature]:checked").val();
+var valFive = $("input:radio[name=house]:checked").val();
