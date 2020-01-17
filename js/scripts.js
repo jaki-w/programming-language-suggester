daa65214bac419002bc79b4ea9eda57790b619dd
Jaki Widby <jaki.widby@gmail.com>
2020-01-17 12:18:50 -0800

    give click functionality to first button that will show hidden page section.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,12 @@ var valTwo = $("input:radio[name=season]:checked").val();
 var valThree = $("input:radio[name=shape]:checked").val();
 var valFour = $("input:radio[name=creature]:checked").val();
 var valFive = $("input:radio[name=house]:checked").val();
+var score = valOne+valTwo+valThree+valFour+valFive;
+
+$(document).ready(function() {
+  $("#firstButton").click(function() {
+    $("#quiz").show();
+  });
+
+
+});
