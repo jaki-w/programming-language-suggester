d610db663e133d41f7810b861a35b09c08e2faa3
Jaki Widby <jaki.widby@gmail.com>
2020-01-17 13:14:54 -0800

    add click functionality to thirdButton that shows/hides sections and .texts name to nameInput span.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-var name = $("input#name").val();
 var valOne = $("input:radio[name=toothbrush]:checked").val();
 var valTwo = $("input:radio[name=season]:checked").val();
 var valThree = $("input:radio[name=shape]:checked").val();
@@ -18,5 +17,12 @@ $(document).ready(function() {
     $("#questions").show();
   });
 
+  $("#thirdButton").click(function() {
+    event.preventDefault();
+    var name = $("input#name").val();
+    $("#questions").hide();
+    $("#suggestion").show();
+    $("#nameInput").text(name);
+  });
 
 });
