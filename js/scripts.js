f6aa9597eb5552e0b87d0d7f92a07f83a1088bbb
Jaki Widby <jaki.widby@gmail.com>
2020-01-17 13:44:04 -0800

    move vars into last function and add branching to determine language suggestion.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,3 @@
-var valOne = $("input:radio[name=toothbrush]:checked").val();
-var valTwo = $("input:radio[name=season]:checked").val();
-var valThree = $("input:radio[name=shape]:checked").val();
-var valFour = $("input:radio[name=creature]:checked").val();
-var valFive = $("input:radio[name=house]:checked").val();
-var score = valOne+valTwo+valThree+valFour+valFive;
-
 $(document).ready(function() {
   $("#firstButton").click(function() {
     $("#intro").hide();
@@ -23,6 +16,23 @@ $(document).ready(function() {
     $("#questions").hide();
     $("#suggestion").show();
     $("#nameInput").text(name);
-  });
 
+    var valOne = parseInt($("input:radio[name=toothbrush]:checked").val());
+    var valTwo = parseInt($("input:radio[name=season]:checked").val());
+    var valThree = parseInt($("input:radio[name=shape]:checked").val());
+    var valFour = parseInt($("input:radio[name=creature]:checked").val());
+    var valFive = parseInt($("input:radio[name=house]:checked").val());
+    var score = valOne+valTwo+valThree+valFour+valFive;
+
+    if (score <= 9) {
+      $("#languageSuggestion").text("JavaScript");
+    }
+    else if (score > 9 && score <= 12) {
+      $("#languageSuggestion").text("Ruby");
+    }
+    else if (score > 12 && score <=15) {
+      $("#languageSuggestion").text("C#");
+    }
+
+  });
 });
